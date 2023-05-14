fa05e48d6e5d7557849b9f888c8511581be59f74
Fran Kezic <kezicfran@gmail.com>
2023-05-14 11:24:33 +0300

    lol-match-data route

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Fran\'s API!' });
