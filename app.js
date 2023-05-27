6e3eeed034260cdf95f37205a9d0871b08d36e52
Fran Kezic <kezicfran@gmail.com>
2023-05-27 18:40:42 +0300

    champ icons pngs stored locally

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const fs_1 = __importDefault(require("fs"));
-const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.get('/', (req, res) => {
+var express = require("express");
+var fs = require("fs");
+var path = require("path");
+var cors = require("cors");
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.get('/', function (req, res) {
     res.json({ message: 'Welcome to Fran\'s API!' });
 });
-const wordleData = JSON.parse(fs_1.default.readFileSync('wordle.json', 'utf-8'));
-app.get('/wordle', (req, res) => {
+var wordleData = JSON.parse(fs.readFileSync('wordle.json', 'utf-8'));
+app.get('/wordle', function (req, res) {
     res.json(wordleData);
 });
-const matchData = JSON.parse(fs_1.default.readFileSync('gamesdata.json', 'utf-8'));
-app.get('/lol-match-data', (req, res) => {
+var matchData = JSON.parse(fs.readFileSync('gamesdata.json', 'utf-8'));
+app.get('/lol-match-data', function (req, res) {
     res.json(matchData);
 });
-app.listen(PORT, () => {
+app.get('/get-draft', function (req, res) {
+    var randomNum = Math.floor(Math.random() * Object.entries(matchData).length);
+    res.json(Object.entries(matchData)[randomNum]);
+});
+app.get('/champion-icons/:championName', function (req, res) {
+    var championName = req.params.championName;
+    var imagePath = path.join(__dirname, 'champion_icons', "".concat(championName, ".png"));
+    fs.readFile(imagePath, function (err, data) {
+        if (err) {
+            console.error(err);
+            res.status(404).json({ error: 'Image not found' });
+        }
+        else {
+            res.set('Content-Type', 'image/png');
+            res.send(data);
+        }
+    });
+});
+app.listen(PORT, function () {
     console.log('Server running on port', PORT);
 });
--- app.ts
+++ app.ts
@@ -1,24 +1,26 @@
-import express, { Request, Response } from 'express'
-import fs from 'fs'
-import cors from 'cors'
+import express = require('express');
+import { Request, Response } from 'express'
+import fs = require('fs');
+import path = require('path');
+import cors = require('cors');
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Welcome to Fran\'s API!'})
 })
 
-const wordleData = JSON.parse(fs.readFileSync('wordle.json', 'utf-8'))
+const wordleData = JSON.parse(fs.readFileSync('wordle.json', 'utf-8'));
 
 app.get('/wordle', (req, res) => {
     res.json(wordleData)
 })
 
-const matchData = JSON.parse(fs.readFileSync('gamesdata.json', 'utf-8'))
+const matchData = JSON.parse(fs.readFileSync('gamesdata.json', 'utf-8'));
 
 app.get('/lol-match-data', (req, res) => {
     res.json(matchData)
@@ -27,6 +29,21 @@ app.get('/lol-match-data', (req, res) => {
 app.get('/get-draft', (req, res) => {
     let randomNum = Math.floor(Math.random() * Object.entries(matchData).length)
     res.json(Object.entries(matchData)[randomNum])
+})
+
+app.get('/champion-icons/:championName', (req, res) => {
+    const championName = req.params.championName;
+    const imagePath = path.join(__dirname, 'champion_icons', `${championName}.png`);
+    
+    fs.readFile(imagePath, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(404).json({ error: 'Image not found' })
+      } else {
+        res.set('Content-Type', 'image/png');
+        res.send(data);
+      }
+    })
 })
 
 app.listen(PORT, () => {
